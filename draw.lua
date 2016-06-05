f2552ceff4106b5e097cad80fb14b67d191bdce7
lukdavis <luke.davis@starcomww.com>
2016-06-04 22:56:16 -0400

    made map reactive to width + height

--- draw.lua
+++ draw.lua
@@ -1,6 +1,4 @@
-function draw_city_map()
-    local screen_width = love.graphics.getWidth()
-    local screen_height = love.graphics.getHeight()
+function draw_city_map(origin_x, origin_y, width, height)
 
     local alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
@@ -9,7 +7,7 @@ function draw_city_map()
 
     for k, v in pairs(state.tiles) do
         local letter = string.find(alphabet, string.sub(k, 1, 1))
-        local num = string.sub(k, 2, 2)
+        local num = string.sub(k, 2, #k)
         table.insert(tile_letters, letter)
         table.insert(tile_nums, num)
     end
@@ -17,33 +15,52 @@ function draw_city_map()
     local grid_width = math.max(unpack(tile_nums))
     local grid_height = math.max(unpack(tile_letters))
 
-    local square_width = screen_width / grid_width
-    local square_height = screen_height / grid_height
+    local square_width = width / grid_width
+    local square_height = height / grid_height
 
     for k, tile in pairs(state.tiles) do
         local letter = string.sub(k, 1, 1)
         local y_value = string.sub(k, 2, #k) - 1
         local x_value = string.find(alphabet, letter) - 1
 
-        local box_origin_x = x_value * square_width
-        local box_origin_y = y_value * square_height
+        local box_origin_x = origin_x + x_value * square_width
+        local box_origin_y = origin_y + y_value * square_height
 
         love.graphics.setColor(255, 255, 255, 255)
 
         -- Draw letters to label grid tiles.
-        love.graphics.print(letter..y_value, box_origin_x + square_width / 2,
+        love.graphics.print(letter..(y_value + 1), box_origin_x + square_width / 2,
             box_origin_y)
         -- Draw grid tiles.
         love.graphics.rectangle("line",
             box_origin_x,
             box_origin_y,
-            box_origin_x + square_width,
-            box_origin_y + square_height)
+            square_width,
+            square_height)
 
         -- Draw values for cost.
         love.graphics.setColor(100, 255, 100, 255)
         love.graphics.print(tile.cost, box_origin_x, box_origin_y + 15)
         love.graphics.setColor(255, 100, 100, 255)
         love.graphics.print(tile.building_type, box_origin_x, box_origin_y + 30)
+
+        -- Draw in-progress building.
+        if tile.is_started and not tile.is_completed then
+            local progress = tile.elapsed_construction_time / tile.construction_time
+            love.graphics.setColor(100, 150, 200, progress * 150)
+            love.graphics.rectangle(box_origin_x, box_origin_y + box_height,
+                square_width / 3, box_height - (box_height * progress))
+        end
     end
+    --return(tile_table)
+end
+
+-- Takes in a string giving a cell name and returns that cell's xy position on screen.
+function get_cell_position(cell, tile_table)
+    local letter = string.sub(cell, 1, 1)
+    local y_value = string.sub(cell, 2, #cell) - 1
+    local x_value = string.find(alphabet, letter) - 1
+
+    local box_origin_x = origin_x + x_value * square_width
+    local box_origin_y = origin_y + y_value * square_height
 end
